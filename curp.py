39ec400b85be71e4d4dd8e8280229cfd996d9ab2
Daniel Garcia <danielgg1103@gmail.com>
2022-06-05 19:51:30 -0500

    programas completos

--- curp.py
+++ curp.py
@@ -1,169 +1,126 @@
 #Nombre del programa: calcCurp.py
 #Autor: Daniel Garcia
-#Fecha: 31/05/22
+#Fecha: 05/06/22
 #Descripcion del programa: Programa que calcula el CURP con los datos de una persona
 
+#biblioteca que contiene la función para limpiar el contenido de la pantalla
 import os
 
 def curp():
     
-    #Diccionario que contiene las entidades federativas de Mexico y sus respectivas abreviaciones
-    entidades = {
-    1:['1.Aguascalientes','AS'], 
-    2:['2.Baja California','BC'], 
-    3:['3.Baja California Sur','BS'],
-    4:['4.Campeche','CC'], 
-    5:['5.Chiapas','CS'], 
-    6:['6.Chihuahua','CH'], 
-    7:['7.Coahuila','CL'],
-    8:['8.Colima','CM'], 
-    9:['9.Ciudad de Mexico','DF'], 
-    10:['10.Durango','DG'], 
-    11:['11.Guanajuato','GT'],
-    12:['12.Guerrero','GR'], 
-    13:['13.Hidalgo','HG'], 
-    14:['14.Jalisco','JC'], 
-    15:['15.Estado de Mexico','MC'],
-    16:['16.Michoacan','MN'], 
-    17:['17.Morelos','MS'], 
-    18:['18.Nayarit','NT'], 
-    19:['19.Nuevo Leon','NL'],
-    20:['20.Oaxaca','OC'], 
-    21:['21.Puebla','PL'], 
-    22:['22.Queretaro','QO'],
-    23:['23.Quintana Roo','QR'],
-    24:['24.San Luis Potosi','SP'], 
-    25:['25.Sinaloa','SL'], 
-    26:['26.Sonora','SR'], 
-    27:['27.Tabasco','TC'],
-    28:['28.Tamaulipas','TS'], 
-    29:['29.Tlaxcala','TL'], 
-    30:['30.Veracruz','VZ'], 
-    31:['31.Yucatan','YN'], 
-    32:['32.Zacatecas','ZS']
-    }
-    
-    
+    #Nombre completo de la persona
     nombre=input("Escriba su nombre: ").upper()
     aPaterno=input("Escriba su apellido paterno: ").upper()
     aMaterno=input("Escriba su apellido materno: ").upper()
-        
 
+    #Lista con las consonantes
+    consonantes = [
+        "B","C","D","F","G",
+        "H","J","K","L","M",
+        "N","Ñ","P","Q","R",
+        "S","T","V","W","X","Y","Z"]
+    
+    #Selecciona la primer vocal del apellido paterno
+    vocalApaterno=""
+    for letra in aPaterno:
+        if letra not in consonantes:
+            vocalApaterno=vocalApaterno+letra
+    vocalAP = vocalApaterno[0:1]
+
+    #Fecha de nacimiento de la persona
     dia=input("Dia de nacimiento: ")
     mes=input("Mes de nacimiento: ")
     anio=input("Año de nacimiento: ")
         
         
-    genero=input("¿Cuál es su género?(Hombre o Mujer) ").upper()
+    genero=input("¿Cuál es su género?(H:hombre M:mujer) ").upper()
     
+    #Limpiar el contenido de la pantalla (solo funciona en linux)
     os.system ("clear")
 
-    for x in entidades:
-        print(entidades[x][0])
-
-    entidadFederativa=int(input("¿En qué entidad federativa nació? "))
+    #Diccionario con las abreviaturas de las entidades federativas
+    entidades = { 
+        '1':'AS', '2':'BC','3': 'BS', '4':'CC','5':'CS',
+        '6':'CH','7':'DF','8':'CL','9':'CM','10':'DG',
+        '11':'GT','12':'GR','13':'HG','14':'JC','15':'MC',
+        '16':'MN','17': 'MS', '18': 'NT','19':'NL','20':'OC',
+        '21':'PL','22':'QO','23':'QR','24':'SP','25':'SL',
+        '26':'SR','27':'TC','28':'TS','29':'TL','30':'VZ',
+        '31':'YN','32':'ZS'}
     
-    if entidadFederativa == 1:
-        entidadFederativa = entidades[1][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 2:
-        entidadFederativa = entidades[2][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 3:
-        entidadFederativa = entidades[3][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 4:
-        entidadFederativa = entidades[4][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 5:
-        entidadFederativa = entidades[5][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 6:
-        entidadFederativa = entidades[6][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 7:
-        entidadFederativa = entidades[7][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 8:
-        entidadFederativa = entidades[8][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 9:
-        entidadFederativa = entidades[9][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 10:
-        entidadFederativa = entidades[10][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 11:
-        entidadFederativa = entidades[11][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 12:
-        entidadFederativa = entidades[12][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 13:
-        entidadFederativa = entidades[13][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 14:
-        entidadFederativa = entidades[14][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 15:
-        entidadFederativa = entidades[15][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 16:
-        entidadFederativa = entidades[16][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 17:
-        entidadFederativa = entidades[17][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 18:
-        entidadFederativa = entidades[18][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 19:
-        entidadFederativa = entidades[19][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 20:
-        entidadFederativa = entidades[20][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 21:
-        entidadFederativa = entidades[21][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 22:
-        entidadFederativa = entidades[22][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 23:
-        entidadFederativa = entidades[23][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 24:
-        entidadFederativa = entidades[24][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 25:
-        entidadFederativa = entidades[25][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 26:
-        entidadFederativa = entidades[26][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 27:
-        entidadFederativa = entidades[27][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 28:
-        entidadFederativa = entidades[28][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 29:
-        entidadFederativa = entidades[29][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 30:
-        entidadFederativa = entidades[30][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 31:
-        entidadFederativa = entidades[31][1]
-        aEntidad = entidadFederativa
-    elif entidadFederativa == 32:
-        entidadFederativa = entidades[32][1]
-        aEntidad = entidadFederativa
- 
+    print("\t***Estados de la República Mexicana***")
+    print (' 1 => Aguascalientes \n', 
+	    '2 => Baja California \n',
+		'3 => Baja California Sur \n',
+		'4 => Campeche \n',
+		'5 => Chiapas \n',
+		'6 => Chihuahua \n',
+		'7 => Ciudad de México \n',
+		'8 => Coahuila de Zaragoza \n',
+		'9 => Colima \n',
+		'10 => Durango \n',
+		'11 => Guanajuato \n',
+		'12 => Guerrero \n',
+		'13 => Hidalgo \n',
+		'14 => Jalisco \n',
+		'15 => México \n',
+		'16 => Michoacán de Ocampo \n',
+		'17 => Morelos \n',
+		'18 => Nayarit \n',
+		'19 => Nuevo León \n',
+		'20 => Oaxaca \n',
+		'21 => Puebla \n',
+		'22 => Querétaro de Arteaga \n',
+		'23 => Quintana Roo \n',
+		'24 => San Luis Potosí \n',
+		'25 => Sinaloa \n',
+		'26 => Sonora \n',
+		'27 => Tabasco \n',
+		'28 => Tamaulipas \n',
+		'29 => Tlaxcalteca \n',
+		'30 => Veracruz-Llave \n',
+		'31 => Yucatán \n',
+		'32 => Zacatecas \n',
+			)
+			
+    claveEstado = input('Ingresa el numero del estado en que naciste (1-32): ')
+    nombreEstado = entidades[claveEstado]
+
+    #Lista con las vocales
+    vocales = ["A","E","I","O","U"]
+    
+    #Selecciona la primer consonante del apellido paterno
+    recAp = aPaterno[1:12]
+    consonanteApaterno=""
+    for letra in recAp:
+        if letra not in vocales:
+            consonanteApaterno=consonanteApaterno+letra
+    consonanteAP = consonanteApaterno[0:1]
+
+
+    #Selecciona la primer consonante del apellido materno
+    recAm = aMaterno[1:12]
+    consonanteAmaterno=""
+    for letra in recAm:
+        if letra not in vocales:
+            consonanteAmaterno=consonanteAmaterno+letra
+    consonanteAM = consonanteAmaterno[0:1]
+
+
+    #Selecciona la primer consonante del nombre
+    recNom = nombre[1:12]
+    consonanteNom=""
+    for letra in recNom:
+        if letra not in vocales:
+            consonanteNom=consonanteNom+letra
+    consonanteN = consonanteNom[0:1]
+
+    #Diferenciador Homoclave
     diferenciadorH=str(0)
+    #Digito verificador
     digitoV=str(9)
 
-    print("Su CURP es: ", aPaterno[0:2]+aMaterno[0]+nombre[0]+anio[2:4]+mes[0:2]+dia[0:2]+genero[0]+entidadFederativa+aPaterno[2]+aMaterno[2]+nombre[1]+diferenciadorH+digitoV)    
+    print("Su CURP es: ", aPaterno[0]+vocalAP+aMaterno[0]+nombre[0]+anio[2:4]+mes[0:2]+dia[0:2]+genero[0]+nombreEstado+consonanteAP+consonanteAM+consonanteN+diferenciadorH+digitoV)    
 
 #Flujo principal
 curp()
---
+++ programaEjemplo.py
@@ -0,0 +1,120 @@
+# Autor: Valencia Olmos Leonel Jordan
+# Fecha: 26 de mayo de 2022
+# Archivo: conocerCURP.py
+# Funcion de generar CURP
+
+#Definir variables 
+#Convertimos las variables en mayusculas con comando .upper()
+nombre = input("Escribe tu(s) nombre(s): ").upper()
+apellido_paterno =input("Escribe tu apellido paterno: ").upper()
+apellido_materno =input("Escribe tu apellido materno: ").upper()
+dia_nacimiento =input("Escribe tu dia de nacimiento: -> dd: ")
+mes_nacimiento =input("Escribe tu mes de nacimiento: -> mm: ")
+año_nacimiento =input("Escribe tu año de nacimiento: -> aaaa: ")
+sexo =input("Digita tu sexo(H/M): ").upper()
+
+#En la CURP se coloca primero la primera letra del apellido paterno
+aPaterno = apellido_paterno[0:1]
+
+#Se coloca la primer vocal del  apellido paterno
+#Para esto se debe eliminar las consonantes del apellido
+consonantes = ["B","C","D","F","G","H","J","K","L","M","N","Ñ","P","Q","R","S","T","V","W","X","Y","Z"]
+vocal_AP=""
+for letra in apellido_paterno:
+    if letra not in consonantes:
+        vocal_AP=vocal_AP+letra
+vAP = vocal_AP[0:1]
+
+#Creamos nuestra funcion para iniciar a crear el CURP
+#Sacamos los caracteres necesarios para el CURP
+def generacion_curp(nombre,apellido_materno,apellido_paterno,dia_nacimiento,mes_nacimiento,año_nacimiento,sexo):
+    nombre = nombre[0:1]
+    apellido_materno = apellido_materno[0:1]
+    año_nacimiento = año_nacimiento[2:4]
+#Juntamos todos los caracteres
+    curp = apellido_materno+nombre+año_nacimiento+mes_nacimiento+dia_nacimiento+sexo
+#Retornamos el CURP a la funcion principal
+    return curp
+    
+#Necesitamos la clave de los estados para continuar
+#Se mostrara un listado
+print ("A continuacion se mostrara un listado con los estados")
+print (' 1 => Aguascalientes \n', 
+	    '2 => Baja California \n',
+		'3 => Baja California Sur \n',
+		'4 => Campeche \n',
+		'5 => Chiapas \n',
+		'6 => Chihuahua \n',
+		'7 => Ciudad de México \n',
+		'8 => Coahuila de Zaragoza \n',
+		'9 => Colima \n',
+		'10 => Durango \n',
+		'11 => Guanajuato \n',
+		'12 => Guerrero \n',
+		'13 => Hidalgo \n',
+		'14 => Jalisco \n',
+		'15 => México \n',
+		'16 => Michoacán de Ocampo \n',
+		'17 => Morelos \n',
+		'18 => Nayarit \n',
+		'19 => Nuevo León \n',
+		'20 => Oaxaca \n',
+		'21 => Puebla \n',
+		'22 => Querétaro de Arteaga \n',
+		'23 => Quintana Roo \n',
+		'24 => San Luis Potosí \n',
+		'25 => Sinaloa \n',
+		'26 => Sonora \n',
+		'27 => Tabasco \n',
+		'28 => Tamaulipas \n',
+		'29 => Tlaxcalteca \n',
+		'30 => Veracruz-Llave \n',
+		'31 => Yucatán \n',
+		'32 => Zacatecas \n',
+			)
+			
+estado = { '1':'AS', '2':'BC','3': 'BS', '4':'CC','5':'CS','6':'CH','7':'DF','8':'CL','9':'CM','10':'DG','11':'GT','12':'GR','13':'HG','14':'JC','15':'MC','16':'MN','17': 'MS', '18': 'NT','19':'NL','20':'OC','21':'PL','22':'QO','23':'QR','24':'SP','25':'SL','26':'SR','27':'TC','28':'TS','29':'TL','30':'VZ','31':'YN','32':'ZS'}
+print("Selecciona el estado donde naciste")
+claveEstado = input('Ingresa el numero del estado (1-32): ')
+nombreEstado = estado[claveEstado]
+
+lugarNacimiento=nombreEstado
+
+#Imprimir la primer consonante del primer apellido
+#No se cuentan las letras ya utilizadas anteriormente
+recAp = apellido_paterno[1:12]
+vocales = ["A","E","I","O","U"]
+consonante_AP=""
+for letra in recAp:
+    if letra not in vocales:
+        consonante_AP=consonante_AP+letra
+cAP = consonante_AP[0:1]
+
+
+#Imprimir la primer consonante de el segundo apellido
+#No se cuentan las letras ya utilizadas anteriormente
+recAm = apellido_materno[1:12]
+vocales = ["A","E","I","O","U"]
+consonante_AM=""
+for letra in recAm:
+    if letra not in vocales:
+        consonante_AM=consonante_AM+letra
+cAM = consonante_AM[0:1]
+
+
+#Imprimir la primer consonante del primer nombre
+#No se cuentan las letras ya utilizadas anteriormente
+recNom = nombre[1:12]
+vocales = ["A","E","I","O","U"]
+consonante_Nom=""
+for letra in recNom:
+    if letra not in vocales:
+        consonante_Nom=consonante_Nom+letra
+cNom = consonante_Nom[0:1]
+
+#Homoclave de 2 caracteres unicos para cada persona
+#En este caso solo se respresentara con asteriscos
+homoclave = "**"
+
+#Por ultimo imprimimos nuestro CURP completo llamando a la funcion
+print(f"\n Tu CURP es:{aPaterno+vAP+generacion_curp(nombre,apellido_materno,apellido_paterno,dia_nacimiento,mes_nacimiento,año_nacimiento,sexo)+lugarNacimiento+cAP+cAM+cNom+homoclave}")
